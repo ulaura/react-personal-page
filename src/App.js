4e000c0dfe9085c29ebef4312e9de706a31fb3b4
Laura Unaeze <l.unaeze5@gmail.com>
2018-03-13 05:16:27 -0700

    Changed the NavBar. Using <Link to=#> instead of <a href=#> for routing.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 
@@ -13,9 +13,9 @@ const App = () => {
       <div>
         <NavBar />
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/portfolio' component={Portfolio} />
-          <Route exact path='/contact' component={Contact} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </div>
     </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
-      <a href="/">Home</a>
-      <a href="/portfolio">Portfolio</a>
-      <a href="/contact">Contact</a>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
     </div>
   );
 };
